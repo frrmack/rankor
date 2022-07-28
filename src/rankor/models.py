4fbac214d679b3360dcbc54a39572c0510fba654
Irmak Sirer <irmak.sirer@gmail.com>
2022-07-28 19:40:24 +0300

    Model update: Every Fight belongs to a single RankedList

--- src/rankor/models.py
+++ src/rankor/models.py
@@ -85,6 +85,9 @@ class Thing(MongoModel):
 class Fight(MongoModel):
     """
     Ranking is done by pairwise comparisons. A Fight is a pairwise comparison.
+    A Fight's result only makes sense in the context of a given RankedList.
+    Every Fight is part of a RankedList and affects the scores of the things
+    within that RankedList.
     Each Fight depicts a comparison of two Things. It keeps information about 
     what the two compared Things were, and what the result was. If there is
     a winner (not a draw), the winner's id is also saved as a convenience to
@@ -92,18 +95,19 @@ class Fight(MongoModel):
     Available results are "FIRST_THING_WINS" (winner is the first element of
     the fighting_things list which is a list with two items), "SECOND_THING_WINS" 
     (winner is the last element of fighting_things) or "DRAW" (a tie).
-    There may be multiple Fights between two things. One such usecase: a user keeps 
-    doing comparisons at different times, and the same couple comes up for comparison
-    multiple times -- the result may be the same every time, or maybe different based 
-    on their mood and thinking at different times. Another usecase: multiple users are
-    voting on Fights within the same RankedList, and different users have
-    different opinions on which Thing is better, hence different results in multiple
-    Fights between the same pair of Things.
+    There may be multiple Fights between two things in a single RankedList. One 
+    such usecase: a user keeps doing comparisons at different times for a RankedList,
+    and the same couple of things comes up for comparison multiple times -- the result 
+    may be the same every time, or maybe different based on their mood and thinking at 
+    different times. Another usecase: multiple users are voting on Fights within the 
+    same RankedList, and different users have different opinions on which Thing is better, 
+    hence different results in multiple Fights between the same pair of Things.
     Think of a Fight as a chess game between two players. The same players may play 
     multiple games and get different results, even if one is a stronger player. The 
     stronger player will likely win more of the Fights, but not necessarily all, and 
     they may even lose the majority just by random chance -- even if that's less likely.
     """
+    ranked_list: PyObjectId
     fighting_things: conlist(PyObjectId, min_items=2, max_items=2) 
     result: Literal['FIRST_THING_WINS', 'SECOND_THING_WINS', 'DRAW']
     winner: Optional[PyObjectId]
