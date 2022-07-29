ba75dba65034f1bda6a87093787ee21b2dc094f5
Irmak Sirer <irmak.sirer@gmail.com>
2022-07-29 15:22:40 +0300

    new model: Score (inherits BaseModel not MongoModel, since it's not stored separately in a collection in the db)

--- settings.py
+++ settings.py
@@ -1,6 +1,43 @@
-# THESE ARE THE MANUAL SETTINGS FOR A SPECIFIC INSTANCE OF THE RANKOR APP
+# THESE ARE THE MANUAL SETTINGS FOR A SPECIFIC INSTANCE OF THE RANKOR API
 
 
+# Database connection
 MONGO_DATABASE_URI = 'mongodb://localhost:27017/rankor'
 
+# Response pagination
 NUMBER_OF_ITEMS_IN_EACH_RESPONSE_PAGE = 20
+
+# TrueSkill mu and sigma initialization values for Things that did not
+# fight yet. These will determine what the score ranges will be roughly.
+# You can think of the 99% possible score range for a new Thing to be
+# MU  +- 3 SIGMA
+# For example, if mu is 2.5 and sigma is 0.84, we are thinking that our
+# prior, our initial Gaussian estimate for the 'underlying true score' of
+# a Thing will be quite likely somewhere between 0 and 5, a lot more likely
+# to be around the center of that range than near 0 or 5. As we gather 
+# information about this Thing with more and more fights, its mu will get 
+# closer to the 'underlying true score' as its sigma gets smaller and smaller.
+# It may end up outside that initial prior range, too, actually, if it keeps
+# losing (or winning) all its fights, but this general way of thinking around 
+# mu and sigma should give you a general idea of the score ranges you are playing in.
+# The larger you choose sigma to be relative to mu, the less initial information
+# you are using in the prior (wider range, flatter shape closer to uniform distribution)
+# The flatter and wider you make this prior, though (large sigmas compared to mu), the
+# more fights you will need before your posteriors get narrow enough to trust.
+# Basically, the less confidence you show in the beginning (initialization), the 
+# more information you'll need to gather to gain enough confidence. Another way of
+# thinking about this is: At the end, for a confident ranking, you want the sigmas
+# to be low. If you start with very high sigmas, it will take longer to get the
+# the desired low sigmas (narrow posteriors). If you start with very low sigmas
+# on the other hand, it's like starting with already strong ideas about all Thing's
+# levels, and it will be hard to differentiate them. 
+# Depending on how many Things you are ranking and how many fights you can afford
+# to complete will lead to different values to be more suitable, but really, don't
+# worry about this too much, the prior's impact on your ranking will be negligible 
+# as long as you have a good amount of fights and the posterior is determined by
+# the data. To give you a very rough idea, by the time you have about ten fights 
+# per Thing in your RankedList, these initial values pretty much won't have any
+# consequences left, and even at smaller numbers of fights, you don't need to worry
+# about these in a lot of cases. 
+DEFAULT_INITIAL_SCORE_MU_VALUE = 50
+DEFAULT_INITIAL_SCORE_SIGMA_VALUE = 15
--- src/rankor/models.py
+++ src/rankor/models.py
@@ -26,6 +26,9 @@ from datetime import datetime
 from src.rankor.pyobjectid import PyObjectId
 
 
+# Import settings for a specific instance of the rankor api (settings.py in the root dir)
+import settings
+
 class MongoModel(BaseModel):
     """
     Parent class for all Rankor models to inherit. It handles the bson object
@@ -77,7 +80,7 @@ class Thing(MongoModel):
     image_url: Optional[AnyUrl]
     category: Optional[str]     # Future idea: Turn categories into a class? Also add a tag class?
     extra_data: Optional[Json]
-    date_added: Optional[datetime]
+    date_created: Optional[datetime]
     date_updated: Optional[datetime]
 
 
@@ -130,6 +133,93 @@ class ThingCollection(MongoModel):
     things: List[PyObjectId]
 
 
+class Score(BaseModel):
+    """
+    A Score instance represents the score of a Thing in a specific RankedList.
+    As Things are compared in Fights within the context of a given RankedList,
+    their scores are updated based on the Fight results. Every RankedList
+    has a dictionary mapping it things to their scores. These scores allow them
+    to be ranked. A thing has only one score per each RankedList it participates in, 
+    and each RankedList has only one score for each thing participating in it. 
+
+    Rankor uses the TrueSkill Bayesian inference algorithm to calculate ranking
+    scores for Things based on Fight results. A Score has the mu and sigma values 
+    of the TrueSkill Rating. TrueSkill uses a Gaussian posterior for estimating 
+    the strength of a Thing (for ranking purposes). mu is the mean of this posterior,
+    it's our current best guess for a Thing's strength. sigma is its standard
+    deviation. The higher the sigma, the less sure we are that the Thing's strength
+    is indeed very near mu. We know with 99% confidence that the Thing's strength is
+    within the range of (mu - 3*sigma) and 
+    
+    It also reports a min_possible_score, which is the equivalent of a TrueSkill 
+    score that Microsoft uses for ranking users based on their skill in competitive 
+    games. They also use this for matchmaking.The min_possible_score is (mu - 3 * sigma). 
+    We are 99% sure that the Thing's strength is at least this min_possible_score. 
+    It can definitely be higher. It also could actually be lower than this but that's 
+    a very low probability. So the 'min_possible_score' name is technically misleading,
+    but accurate enough in practice.
+    Why is Microsoft using this? As players play more (or Things fight more), the 
+    uncertainty around their skill level goes down, they have smaller sigmas. For
+    players / Things that didn't fight a lot yet, min_possible_score will be low due
+    to high sigma values. That's desired. If a min_possible_score is high, you know
+    that it's really high due to information from their fights, and not just because
+    of a small number of data points. Think about it like this: On Amazon, would you
+    buy a product that has 12000 reviews with an average of 4.1 stars, or another one
+    that has 5 reviews with an average of 4.2 stars? With the first one, you feel a lot
+    more certain of the average star rating due to the large number of reviews. This is
+    aking to a Thing that has been in many Fights and therefore has a low sigma. The latter
+    is like a Thing that only has been in a few Fights. The former would have a min_possible_score
+    that actually is close to 4.1 (if we are using stars as our score unit), whereas the
+    min_possible_score of the latter could still be at 2.5 stars or something like that,
+    since there still is a ton of uncertainty / high sigma due to lack of review data.
+    Microsoft has chosen to value this a lot. It ranks people based on the 99% certainty 
+    about their minimum skill level. This also creates the desired experience that a new player
+    is ranked low at the beginning (due to high sigma), they they rise up the ranks
+    
+    While Rankor provides the min_possible_score value usually used with TrueSkill rankings,
+    it also reports its own approach to reducing mu and sigma to a single score to sort by 
+    for ranking. This is because the ranking it wants to achieve is a different problem than
+    leaderboards for multiplayer games or the experience design for a new player in both 
+    matchmaking and rising up the ranks. Rankor calculates a rankor_score, and recommends
+    using this for ranking. The rankor_score is (mu-sigma). The same idea of valuing high data,
+    low sigma idea is still considered, but instead of the 99% certainty level, a 68% certainty
+    level is preferred. rankor_score still punishes high sigma, but to a much lower extent than
+    Miscrosoft TrueSkill rankings. One of the most important reasons for this is the difference
+    in sigma variance. In Microsoft's case, some players just play the game a lot, and others 
+    don't, which means there are some players with very high sigma, some with middle values, some
+    with very low, etc. Sigma values vary a lot. Whereas in Rankor, Things do not have different
+    preferred fighting frequencies, the number of fights that each Thing will experience in a 
+    RankedList will be a lot more comparable (with some noise of course). Both because each Thing
+    is 'willing' to participate to the same degree unlike game players, and also because of the 
+    matchmaking. When matching players to play a game, Microsoft tries to optimize for similar
+    skill levels, meaning they match people that are likeliest to draw -- that's to avoid a pro
+    dominating a newbie situation. But Rankor optimizes to maximize information gain. This means
+    that if we know less about a Thing (high sigma), that Thing is more likely to be called to 
+    a new fight: A fight involving them will give us a lot more new information than a fight between
+    two veteran Things that have already been in hundreds of fights---we already know a lot about them,
+    their sigmas are low, another fight data point wouldn't teach us much more. Therefore, rankor's
+    matchmaking keeps sigma variance low among all Things. This means we have to worry less about a 
+    12000 reviews 4.1 stars vs 5 reviews 4.2 stars situation, the review numbers of products would be
+    comparable in this analogy. This gives us the chance to use a value closer to mu (our best guess),
+    therefore while rankor still adjusts by going ine stdev below the mean, it is less cautious than
+    Microsoft due to the different nature of the problem and matchmaking.
+
+    You can of course design your own ranking score calculation using mu or sigma, or you may prefer
+    to directly rank Things by mu (our best guess for their underlying 'true' score). rankor_score
+    is only what this rankor api suggests as a good ranking score design.
+    """
+    mu    = settings.DEFAULT_INITIAL_SCORE_MU_VALUE
+    sigma = settings.DEFAULT_INITIAL_SCORE_SIGMA_VALUE
+
+    @property
+    def min_possible_score(self) -> float:
+        return self.mu - 3 * self.sigma
+
+    @property
+    def rankor_score(self) -> float:
+        return self.mu - self.sigma
+
+
 
 class RankedList(MongoModel):
     """
@@ -145,9 +235,11 @@ class RankedList(MongoModel):
     Things to scores, and the associated Fights that gave rise to those scores.
     """
     name: str
-    collection: PyObjectId                     # The sourcing ThingCollection
+    # collection: PyObjectId                     # The sourcing ThingCollection
     thing_scores: Dict[PyObjectId, float]
     fights: List[PyObjectId]
+    date_created: Optional[datetime]
+    date_updated: Optional[datetime]
 
 
 
