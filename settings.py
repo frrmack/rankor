a7f2d06e76c0e7037e8de7c69f18e098e481f4bb
Irmak Sirer <irmak.sirer@gmail.com>
2022-08-04 14:16:02 +0300

    new setting: ALLOW_MANUAL_EDITING_OF_RANKEDLIST_FIGHTS_OR_SCORES = False

--- settings.py
+++ settings.py
@@ -4,11 +4,34 @@
 # Database connection
 MONGO_DATABASE_URI = 'mongodb://localhost:27017/rankor'
 
+
 # Response pagination
 NUMBER_OF_ITEMS_IN_EACH_RESPONSE_PAGE = 20
 
+
+# Setting to let RankedList edit endpoint accept fights and thing_scores
+# fields
+ALLOW_MANUAL_EDITING_OF_RANKEDLIST_FIGHTS_OR_SCORES = False
+# RankedList edits/updates are limited to their metadata only by default.
+# This means that you can only change their name using the edit endpoint.
+# Fights can be saved to or deleted from RankedLists via other endpoints,
+# and this is normally the only way to influence the fights list and the
+# thing_scores list in a RankedList. If you set the setting above to True, 
+# the RankedList edit endpoint will allow you to directly update these 
+# lists.
+# Doing this is NOT recommended, as it's not a good way of handling the 
+# data, it's prone to introducing slient errors that will come back to 
+# bite you later, and it's easy to mess up unless you know exactly what
+# you're doing.
+
+
 # TrueSkill mu and sigma initialization values for Things that did not
-# fight yet. These will determine what the score ranges will be roughly.
+# fight yet. These define the prior, and they will also determine what 
+# the score ranges will be roughly.
+DEFAULT_INITIAL_SCORE_MU_VALUE = 50
+DEFAULT_INITIAL_SCORE_SIGMA_VALUE = 15
+# These are the mean and standard deviation of the Gaussian prior for 
+# the TrueSkill strength/score estimation.
 # You can think of the 99% possible score range for a new Thing to be
 # MU  +- 3 SIGMA
 # For example, if mu is 2.5 and sigma is 0.84, we are thinking that our
@@ -36,8 +59,8 @@ NUMBER_OF_ITEMS_IN_EACH_RESPONSE_PAGE = 20
 # worry about this too much, the prior's impact on your ranking will be negligible 
 # as long as you have a good amount of fights and the posterior is determined by
 # the data. To give you a very rough idea, by the time you have about ten fights 
-# per Thing in your RankedList, these initial values pretty much won't have any
-# consequences left, and even at smaller numbers of fights, you don't need to worry
-# about these in a lot of cases. 
-DEFAULT_INITIAL_SCORE_MU_VALUE = 50
-DEFAULT_INITIAL_SCORE_SIGMA_VALUE = 15
+# per Thing in your RankedList, these initial values likely won't matter as much
+# (depending on the circumstances), and even at smaller numbers of fights, you 
+# don't need to worry about these in a lot of cases. The best way to make sure
+# is to run a few experiments with different initial values but the same fight
+# results to see if the resulting scores are converging or not.
