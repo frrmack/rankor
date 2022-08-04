c3fdda746f8881a04771f6005e131abb74b0ade4
Irmak Sirer <irmak.sirer@gmail.com>
2022-08-04 13:57:07 +0300

    New endpoint design: /rankor/rankedlists/<ranked_list_id>/scores/

--- src/rankor/__init__.py
+++ src/rankor/__init__.py
@@ -8,13 +8,11 @@ from flask_pymongo import PyMongo
 import settings
 
 
-
 # Configure Flask & Flask-PyMongo
 app = Flask(__name__)
 app.config["MONGO_URI"] = settings.MONGO_DATABASE_URI
 pymongo = PyMongo(app)
 db = pymongo.db
-
 
 
 # Import and register error handlers
@@ -41,7 +39,6 @@ from src.rankor.routing_rules import SlashInsistingRoutingRule
 app.url_rule_class = SlashInsistingRoutingRule
 
 
-
 # Import and register all endpoints
 from src.rankor.thing_endpoints import thing_endpoints
 app.register_blueprint(thing_endpoints)
@@ -49,16 +46,15 @@ app.register_blueprint(thing_endpoints)
 from src.rankor.ranked_list_endpoints import ranked_list_endpoints
 app.register_blueprint(ranked_list_endpoints)
 
-
-# # Thing endpoints: /rankor/things/
-# #
-# # Add a new Thing       |   POST    /rankor/things/
-# # Delete a Thing        |   DELETE  /rankor/things/<thing_id>
-# # Edit/Update a Thing   |   PUT     /rankor/things/<thing_id>
-# # List all Things       |   GET     /rankor/things/     
-# # Show one Thing        |   GET     /rankor/things/<thing_id>
-
-
+#------------------------------------------------------------------------------
+# Thing endpoints: /rankor/things/
+#
+# Add a new Thing       |   POST    /rankor/things/
+# Delete a Thing        |   DELETE  /rankor/things/<thing_id>
+# Edit/Update a Thing   |   PUT     /rankor/things/<thing_id>
+# List all Things       |   GET     /rankor/things/     
+# Show one Thing        |   GET     /rankor/things/<thing_id>
+#------------------------------------------------------------------------------
 # RankedList endpoints: /rankor/rankedlists/
 #
 # Create a new RankedList   |   POST    /rankor/rankedlists/
@@ -66,11 +62,15 @@ app.register_blueprint(ranked_list_endpoints)
 # Edit/Update a RankedList  |   PUT     /rankor/rankedlists/<ranked_list_id>
 # List all RankedLists      |   GET     /rankor/rankedlists/
 # Get a RankedList          |   GET     /rankor/rankedlists/<ranked_list_id>
-
-
+#------------------------------------------------------------------------------
+# Score endpoint for a given RankedList: /rankor/rankedlists/<ranked_list_id>/scores/
+#
+# Get the scores of the ranked Things in a RankedList 
+# GET     /rankor/rankedlists/<ranked_list_id>/scores/
+#------------------------------------------------------------------------------
 # Fight endpoints for a given RankedList: /rankor/rankedlists/<ranked_list_id>/fights/
 #
-# Get a new Fight between two Things in a RankedList  
+# Get a new Fight between two Things in a RankedList (fight matchmaking)
 # GET     /rankor/rankedlists/<ranked_list_id>/fights/new/
 #
 # Save the result of a Fight            
@@ -84,3 +84,4 @@ app.register_blueprint(ranked_list_endpoints)
 #
 # Retrieve all Fights of a Thing in a RankedList 
 # GET     /rankor/rankedlists/<ranked_list_id>/fights/things/<thing_id>
+#------------------------------------------------------------------------------
