1c6e2386bf60702075641b3067812521bd425768
Irmak Sirer <irmak.sirer@gmail.com>
2023-08-18 16:37:53 -0500

    delete test for ranked_lists

--- tests/endpoints/ranked_things/test_deleting_ranked_lists.py
+++ tests/endpoints/ranked_things/test_deleting_ranked_lists.py
@@ -1,7 +1,7 @@
 from urllib.parse import urljoin
 import requests
 
-def test_retrieve_a_ranked_list(server, ranked_lists_endpoint, movie_data):
+def test_delete_a_ranked_list(server, ranked_lists_endpoint, movie_data):
     # create a ranked list
     list_name = "James Cameron Movies"
     new_ranked_list_data_dict = {"name": list_name}
@@ -9,13 +9,16 @@ def test_retrieve_a_ranked_list(server, ranked_lists_endpoint, movie_data):
     assert response.status_code == 200
     response_data = response.json()
     ranked_list_id_str = response_data['ranked_list']['data']['id']
-    # now get it back
+    # now delete it
     endpoint = urljoin(ranked_lists_endpoint, ranked_list_id_str,'/')
-    response = requests.get(endpoint)
+    response = requests.delete(endpoint)
     response_data = response.json()
     assert response_data["http_status_code"] == 200
     assert response_data["result"] == "success"
-    assert response_data["ranked_list"]["data"]["name"] == list_name
-    assert response_data["ranked_list"]["data"]["id"] == ranked_list_id_str
+    # make sure it's gone
+    response = requests.get(endpoint)
+    assert response.status_code == 404
+    assert response.json()["error_type"] == "ResourceNotFoundInDatabaseError"
+
 
  
